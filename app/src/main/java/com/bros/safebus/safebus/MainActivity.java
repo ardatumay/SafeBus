bc0f5245ab579aa6c4eaeeeed246600d9152885b
Arda Tumay <ardatm15@gmail.com>
2018-10-24 19:23:40 +0300

    firebasechanges

--- app/src/main/java/com/bros/safebus/safebus/MainActivity.java
+++ app/src/main/java/com/bros/safebus/safebus/MainActivity.java
@@ -3,11 +3,27 @@ package com.bros.safebus.safebus;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+
+        myRef.setValue("Hello, Worlddddd!");
+
+
+
+
+
+
     }
 }
