019816fedeffa3ed122b23f64ff4fc74511e7615
Arda Tumay <ardatm15@gmail.com>
2018-10-25 13:56:21 +0300

    Firebase read is implemented

--- app/src/main/java/com/bros/safebus/safebus/MainActivity.java
+++ app/src/main/java/com/bros/safebus/safebus/MainActivity.java
@@ -1,29 +1,45 @@
 package com.bros.safebus.safebus;
 
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.HashMap;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference();
+        DatabaseReference myRef = database.getReference("/children/0/busPlate");
 
-        myRef.setValue("Hello, Worlddddd!");
-
-
+        //myRef.setValue("Hello, Worlddddd!");
 
 
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                java.lang.Object value = dataSnapshot.getValue();
+                Log.d("VALUE", "Value is: " + value);
+            }
 
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                    Log.w("VALUE", "Failed to read value.", error.toException());
+            }
+        });
 
     }
 }
