17f9853e2d04129cccf60694b2696f305188043f
Arda Tumay <ardatm15@gmail.com>
2018-11-01 19:49:25 +0300

    location updates is succesfully implemented

--- app/src/main/java/com/bros/safebus/safebus/MainActivity.java
+++ app/src/main/java/com/bros/safebus/safebus/MainActivity.java
@@ -14,7 +14,9 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.Manifest;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -30,22 +32,21 @@ import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FusedLocationProviderClient mFusedLocationClient;
+    private FusedLocationProviderClient mFusedLocationClient; //location provider client
+    LocationRequest mLocationRequest = new LocationRequest(); // location request
     Location currentLoc = null;
+    private LocationCallback mLocationCallback;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView CurrentLocation = (TextView) findViewById(R.id.currentLocation);
-
+        //////////////////////FIREBASE RELATED////////////////////
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("/children/0/busPlate");
-
         //myRef.setValue("Hello, Worlddddd!");
-
-
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -62,47 +63,158 @@ public class MainActivity extends AppCompatActivity {
                 Log.w("VALUE", "Failed to read value.", error.toException());
             }
         });
-        createLocationRequest();
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        //////////////////////FIREBASE RELATED////////////////////
+
+        //bussiness logic variables
+        createLocationRequest(mLocationRequest); //create the location request
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this); //get the location provider client
+
+        //user interface variables
+        final TextView updatedLocation = (TextView) findViewById(R.id.updatedLocation);
+        final TextView CurrentLocation = (TextView) findViewById(R.id.currentLocation);
+        Button showLocation = (Button) findViewById(R.id.showLocation);
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    //Log.d("LOC", location.toString());
+                    // Update UI with location data
+                    // ...
+                    updatedLocation.setText(location.toString());
+                }
+            };
+        };
+
+
+
+
+
+        showLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GetLoc();
+                if (currentLoc == null) {
+                    setTextView(CurrentLocation, "Location Object Is Null");
+
+                } else {
+                    setTextView(CurrentLocation, currentLoc.toString());
+                   //setTextView(CurrentLocation, currentLoc.getLatitude() + " " + currentLoc.getAltitude() + " with accuracy " + currentLoc.getAccuracy() );
+                }
+            }
+        });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        boolean mRequestingLocationUpdates = true;
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+    private void startLocationUpdates() {
+        checkLocationPermission();
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                mLocationCallback,
+                null /* Looper */);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    private void stopLocationUpdates() {
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    void GetLoc(){//get the last location
         checkLocationPermission();
         mFusedLocationClient.getLastLocation()
                 .addOnSuccessListener(this, new OnSuccessListener<Location>() {
                     @Override
                     public void onSuccess(Location location) {
                         // Got last known location. In some rare situations this can be null.
-                        Log.d("LOCATION INFO", "Location info");
-
                         if (location != null) {
                             // Logic to handle location object
-                            Log.d("LOCATION INFO", location.toString());
                             currentLoc = location;
                         }
                     }
                 });
-        Button showLocation = (Button) findViewById(R.id.showLocation);
-        showLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //
-                if (currentLoc == null) {
-                    setTextView(CurrentLocation, "Location Object Is Null");
-
-                } else {
-                    setTextView(CurrentLocation, currentLoc.toString());
-
-                   // setTextView(CurrentLocation, currentLoc.getLatitude() + " " + currentLoc.getAltitude() + " with accuracy " + currentLoc.getAccuracy() );
-                }
-            }
-        });
     }
 
-    void setTextView(TextView targetTextView, String targetString){
+
+
+
+
+
+
+
+
+    void setTextView(TextView targetTextView, String targetString){//set textView content
         targetTextView.setText(targetString);
     }
 
+
+
+
+
+
+
+
+
+
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
 
-    public boolean checkLocationPermission() {
+    public boolean checkLocationPermission() { //check whether the app has enough permissions for location services
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -141,13 +253,42 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
     }
-    protected void createLocationRequest() {
-        LocationRequest mLocationRequest = new LocationRequest();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    protected void createLocationRequest(LocationRequest request) { //create a location request for the location updates
         mLocationRequest.setInterval(10000);
         mLocationRequest.setFastestInterval(5000);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
                 .addLocationRequest(mLocationRequest);
+        builder.build();
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
