c64b5e7fcdfb8ce06e158088788beac892801178
MEHMET CAN ERTuZuN <mcan.ertuzun@tedu.edu.tr>
2018-11-07 19:28:58 +0300

    Map have a problem(location)

--- app/src/main/java/com/bros/safebus/safebus/MainActivity.java
+++ app/src/main/java/com/bros/safebus/safebus/MainActivity.java
@@ -1,7 +1,10 @@
 package com.bros.safebus.safebus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
@@ -15,15 +18,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
+       /* FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference();
 
-        myRef.setValue("Hello, Worlddddd!");
+        myRef.setValue("Hello, Worlddddd!");*/
 
-
-
-
-
+        Button mapsButton = (Button) findViewById(R.id.o_map);
+        mapsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public  void onClick(View v){
+                goMaps();
+            }
+        });
 
     }
+
+    void goMaps (){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/bros/safebus/safebus/MapsActivity.java
+++ app/src/main/java/com/bros/safebus/safebus/MapsActivity.java
@@ -1,12 +1,17 @@
 package com.bros.safebus.safebus;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -34,15 +39,53 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
+        LatLng kralCanınEvi = new LatLng(39.892967, 32.855078);
+        mMap.addMarker(new MarkerOptions().position(kralCanınEvi).title("KRAL CAN"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kralCanınEvi, 18));
+        mMap.addMarker(new MarkerOptions()
+        .position(kralCanınEvi)
+        .title("king")
+        );
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
 
     }
+
+
+    public void mapTypeSelector(String mType) {
+        GoogleMap googleMap = getmMap();
+        setmMap(googleMap);
+        switch(mType){
+            case "Normal": googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL); break;
+            case "Hybrid": googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID); break;
+            case "Setellite": googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE); break;
+            case "Terrain": googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN); break;
+        }
+
+    }
+
+
+    public GoogleMap getmMap() {
+        return mMap;
+    }
+
+    public void setmMap(GoogleMap mMap) {
+        this.mMap = mMap;
+    }
+
 }
