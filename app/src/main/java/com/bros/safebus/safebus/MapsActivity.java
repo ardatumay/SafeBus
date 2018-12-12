e06706a96fdfcfaf644da72050161b39511067ce
MEHMET CAN ERTuZuN <mcan.ertuzun@tedu.edu.tr>
2018-12-13 00:15:25 +0300

    fetchingChild

--- app/src/main/java/com/bros/safebus/safebus/MapsActivity.java
+++ app/src/main/java/com/bros/safebus/safebus/MapsActivity.java
@@ -4,21 +4,32 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -39,6 +50,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private static final int LOCATION_REQUEST = 500;
     ArrayList<LatLng> listPoints;
+    private final FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private String userType = "";
 
 
     @Override
@@ -50,7 +64,49 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         listPoints = new ArrayList<>();
+
+        FirebaseUser currentUser = firebaseAuth.getInstance().getCurrentUser();
+        final String RegisteredUserID = currentUser.getUid();
+
+        final DatabaseReference databaseref = FirebaseDatabase.getInstance().getReference().child("parents").child(RegisteredUserID);
+
+        Log.v("logger", "bossss");
+
+        databaseref.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String as = "";
+                String sa = dataSnapshot.child("children").getValue().toString();
+
+                int tire = sa.indexOf('{');
+                int esittir = sa.indexOf('=');
+               sa = sa.substring(tire + 1 ,esittir );
+                Log.v("logger", sa);
+
+                if(!sa.isEmpty()){
+                    as = dataSnapshot.child("children").child(sa).child("key").getValue().toString();
+                }
+
+               // DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("children").child(as);
+                //String sa = dataSnapshot.child("children").getValue().toString();
+
+                Log.v("logger", sa);
+                Log.v("logger", as);
+
+
+                  //  String lat = dataSnapshot.child("location").child("currentLocation").child("latitude").getValue().toString();
+               //     String longitude = dataSnapshot.child("location").child("currentLocation").child("longitude").getValue().toString();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
+
 
     // Add a marker in Sydney and move the camera
         /*LatLng kralCanınEvi = new LatLng(39.892967, 32.855078, 39.892311, 32.854128);
@@ -79,6 +135,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         2) location değiştiğinde bunu anlaması için call methodu yarat
         3) Değişimi anladığında location arası noktaların arasını google servisi kullanarak bağla
          */
+
 
 
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
